1300a6ed15f474a6c4cb5a0b86d5dc05d1d3d1e9
kianrevale <krevale3@student.rccd.edu>
2026-06-27 00:42:34 -0700

    Added main.cpp and finished code
    Main.cpp along with readme

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main()
+{
+    cout << "Name = Kian Revale" << endl;
+    cout << "Goals in Life = Educational prowess with engineering degrees from USC, become a successful artist/musician, make money and donate to charity" << endl;
+    cout << "Current Interests = music, programming, comedy, food" << endl;
+    cout << "Dreams = Build and own a record store, make a non-profit organization" << endl;
+    cout << "1 year from now = hardworking high-school student, ready to graduate and go to college" << endl;
+    cout << "3 years from now = finished first year of college, seeking internships and researching" << endl;
+    cout << "5 years from now = ready to graduate college with a few years of work, intern experience, and connecting" << endl;
+    cout << "7 years from now = working, providing as much as I can for my family" << endl;
+}
